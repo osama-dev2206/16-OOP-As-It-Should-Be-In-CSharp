34d66d5f9c93b3b5c84acb5bdcb8353c6bdbe3cd
osama-mohamoud2006 <wwwhdosama@gmail.com>
2026-05-21 01:43:23 +0300

    Auto commit: Thu May 21 01:43:23 EDT 2026

---
+++ OOP REV WITH C++/virtualFunction.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Super
+{
+public:
+    virtual void Function()
+    {
+        cout << "Hello From Super Class!\n";
+    }
+};
+
+class Sub : public Super
+{
+public:
+    void Function() override
+    {
+        cout << "Hello From Sub Class!\n";
+    }
+};
+
+int main()
+{
+    Sub s;
+    s.Function();
+
+    Super s1;
+    s1.Function();
+
+    Super *U = &s;
+    U->Function(); 
+}
