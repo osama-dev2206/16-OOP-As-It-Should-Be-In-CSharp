00c154e58a359b45883a6059b369c62d31e1c511
osama-mohamoud2006 <wwwhdosama@gmail.com>
2026-06-19 17:02:05 +0300

    Auto commit: Fri Jun 19 17:02:05 EDT 2026

---
+++ C# OOP OLd Ver/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__OOP_OLd_Ver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Inheritance/IClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+
+    public interface IPerson
+    {
+        // All Method & Properties Are Public By Default In Interface 
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+        void Introduce();
+        void Print();
+
+        string To_String();
+
+        static int y = 10; // allowed in newer vers of c#
+
+
+        // must be public or protected 
+        protected abstract void ui();
+    }
+
+
+    class B : IPerson
+    {
+        void IPerson.ui()
+        {
+            throw new NotImplementedException();
+        }
+        string IPerson.FirstName { get; set;}
+        string IPerson.LastName { get; set;}
+
+        void IPerson.Introduce() 
+            { }
+
+        void IPerson.Print() { }
+
+
+        string IPerson.To_String()
+        { return "";  }
+
+    }
+
+
+    /// <summary>
+    /// ///////////////////////////////////////////////////////////////////
+    /// </summary>
+
+    abstract class clsAbstract
+    {
+        static int y = 10; //abstract classes can have static ... 
+        public clsAbstract() // constructor 
+        {
+
+        }
+        public int MyProperty { get; set; }
+        public void print() // method 
+        {
+            Console.WriteLine("     "); 
+        }
+
+        public abstract void met(); // abstract method you should implement it 
+
+        int m = 10; // you can use data member in abstract class 
+    }
+    
+    class x : clsAbstract
+    {
+       void f()
+        {
+            MyProperty++;  // property from abstract class 
+            print(); 
+        }
+
+        public override void met() // implemented the abstract method 
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
